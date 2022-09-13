2b671a5ee0b6c2fa9c3d506f4285d4019d9bd8f3
Camille Linan <camille.linan@hotmail.com>
2022-09-13 21:48:22 +0200

    add middlewares

---
+++ middlewares/password.middleware.js
@@ -0,0 +1,17 @@
+const passwordValidator = require('password-validator');
+
+const passwordSchema = new passwordValidator()
+
+passwordSchema
+.is().min(4)
+.has().uppercase()
+.has().lowercase()
+.has().digits(1);
+
+module.exports = (req, res, next) => {
+    if (passwordSchema.validate(req.body.password)) {
+        next();
+    } else {
+        return res.status(400).json({ error: `Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et sans espace` });
+    }
+}
